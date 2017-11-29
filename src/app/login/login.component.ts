03c859aa82c0e99c36abbd1d8f0f012ba31891b8
John Shao <shaojianyong@hotmail.com>
2017-11-29 17:42:08 +0800

    Fix bug

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+  FormControl
+} from '@angular/forms';
+
+
+/**
+ * Our custom validator
+ *
+ * A validator:
+ * - Takes a `Control` as it's input and
+ * - Returns a `StringMap<string, boolean>` where the key is "error code" and
+ *   the value is `true` if it fails
+ */
+function usernameValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^123/)) {
+    return { invalidUsername: true };
+  }
+}
+
+function passwordValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^123/)) {
+    return { invalidPassword: true };
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -6,13 +34,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  myForm: FormGroup;
+  username: AbstractControl;
+  password: AbstractControl;
+  errorMsg: string;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'username': ['', Validators.compose([Validators.required])],
+      'password': ['', Validators.compose([Validators.required])],
+      'errorMsg': [''],
+    });
+
+    this.username = this.myForm.controls['username'];
+    this.password = this.myForm.controls['password'];
+    this.errorMsg = 'Hello';
+  }
 
   ngOnInit() {
   }
 
   onSubmit(form: any): void {
     console.log('You submitted value: ', form);
+    this.errorMsg = 'hello world!';
   }
 }
