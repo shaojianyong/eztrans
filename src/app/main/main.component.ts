7fb4a5830c8e3423d0fc7228b9e18c790ac4c3aa
John Shao <shaojianyong@hotmail.com>
2017-12-04 00:38:22 +0800

    Design UI

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import languages from './languages';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+
+  langs = languages;
 
   constructor() { }
 
