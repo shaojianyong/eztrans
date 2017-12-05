a2f8063ae2f5df321308fed7a92f8be097629880
John Shao <shaojianyong@hotmail.com>
2017-12-05 17:36:50 +0800

    temp submmit

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit } from '@angular/core';
+import {Component, OnInit, ReflectiveInjector } from '@angular/core';
 
 const electron = (<any>window).require('electron');
 const ipc = electron.ipcRenderer;
 const dialog = electron.remote.dialog;
+const BrowserWindow = electron.remote.BrowserWindow;
 
 import { ExLinksModule } from '../../assets/ex-links';
 
@@ -13,29 +14,34 @@ import { ExLinksModule } from '../../assets/ex-links';
 })
 export class MainComponent implements OnInit {
 
+  source_text = 'Hello World!';
   sentences = new Array<Object>();
 
   static onFileRead(event, err, data, userData): void {
+
+
+    console.log(userData);
+    console.log(`2 ==> ${mc instanceof MainComponent}`);
     // userData.sentences = [];
-    const lines = data.split(/\n|\r\n/g);
+    /*const lines = data.split(/\n|\r\n/g);
     for (let line of lines) {
       line = line.trim();
       if (line) {
-        console.log(`${userData.sentences.length}: ${line}`);
-        userData.sentences[userData.sentences.length] = { source: line, target: '' };
+        console.log(`${mc.sentences.length}: ${line}`);
+        mc.sentences[mc.sentences.length] = { source: line, target: '' };
       }
-    }
+    }*/
     // userData.cdr.markForCheck();
     // userData.myTest();
   }
 
   constructor() {
-
   }
 
   openFile(): void {
     dialog.showOpenDialog((files) => {
-      ipc.send('read-file', files, this);
+      console.log(`1 ==> ${this instanceof MainComponent}`);
+      ipc.send('read-file', files, 0);
     });
   }
 
