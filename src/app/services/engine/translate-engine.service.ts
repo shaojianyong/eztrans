639aa354a5576ea017addcf5628d3ffb3ee413c2
John Shao <shaojianyong@hotmail.com>
2017-12-19 07:40:04 +0800

    使用过可观察对象

--- src/app/services/engine/translate-engine.service.ts
+++ src/app/services/engine/translate-engine.service.ts
@@ -5,5 +5,5 @@ export interface TranslateEngineService {
   source_lang: string; // = 'en';
   target_lang: string; // = 'zh-cn';
 
-  translate(source_text: string, callback: Function);
+  translate(source_text: string, callback: Function);  // 使用可观察对象
 }
