fb65a09f1b4969607948c734120366841eae355e
John Shao <shaojianyong@hotmail.com>
2017-12-20 07:26:16 +0800

    modify translate model

--- src/app/services/engine/translate.model.ts
+++ src/app/services/engine/translate.model.ts
@@ -4,13 +4,17 @@ export class Translate {
   target_lang: string;
   source_text: string;
   target_text: string;
+  hz_translit: string;
   engine_name: string;
+  trans_state: number;
 
   constructor(obj?: any) {
     this.source_lang = obj && obj.source_lang || 'en';
     this.target_lang = obj && obj.target_lang || 'zh';
     this.source_text = obj && obj.source_text || '';
     this.target_text = obj && obj.target_text || '';
+    this.hz_translit = obj && obj.hz_translit || '';
     this.engine_name = obj && obj.engine_name || 'Google';
+    this.trans_state = obj && obj.trans_state || 0;
   }
 }
