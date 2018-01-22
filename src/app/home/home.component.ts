fc6f998215641d68881a2c4699e7edc962807839
John Shao <shaojianyong@hotmail.com>
2018-01-22 13:16:26 +0800

    temp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,11 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  select(event: MouseEvent): void {
+    console.log(event);
+    $('#my-row').css('background-color', 'gray');
+  }
+
   ngOnInit() {
     $('.ui.accordion')
       .accordion();
