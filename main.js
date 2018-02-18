da5c2f175f9e158204afcb3a1c8815adfe34cecb
John Shao <shaojianyong@hotmail.com>
2018-02-18 19:58:47 +0800

    add BrowserView for main content

--- main.js
+++ main.js
@@ -25,30 +25,31 @@ let openedDocs = {};
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+let view;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   // electron.Menu.setApplicationMenu(null);  // 隐藏菜单栏
   mainWindow = new BrowserWindow({width: 800, height: 600, minWidth: 800, minHeight: 600});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
+  view = new BrowserView();
+  mainWindow.setBrowserView(view);
+  const wcb = mainWindow.getContentBounds();
+  view.setBounds({x: 0, y: 0, width: wcb.width, height: wcb.height});
+  view.setAutoResize({width: true, height: true});
+  view.webContents.loadURL(url.format({
     pathname: path.join(__dirname, 'dist', 'index.html'),
     protocol: 'file:',
     slashes: true
   }));
+  // view.webContents.loadURL('https://www.baidu.com/');
 
-  /*
-  mainWindow.on('close', function () {
-    console.log('Main window close.');
-  });
-  */
-
+  // view.webContents.openDevTools();
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -62,7 +63,7 @@ function createWindow () {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -70,7 +71,7 @@ app.on('window-all-closed', function () {
   }
 });
 
-app.on('activate', function () {
+app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
@@ -81,7 +82,7 @@ app.on('activate', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-function readFile (event, files, group_id) {
+function readFile(event, files, group_id) {
   if (files) {
     const filePath = files[0];
     fs.readFile(filePath, 'utf8', function(err, data) {
@@ -91,7 +92,7 @@ function readFile (event, files, group_id) {
 }
 
 // 同步通信，如果不设置event.returnValue，界面会僵住
-function readFileSync (event, files) {
+function readFileSync(event, files) {
   if (files) {
     const filePath = files[0];
     fs.readFile(filePath, 'utf8', function(err, data) {
@@ -104,7 +105,7 @@ function readFileSync (event, files) {
   }
 }
 
-function saveFile (event, filePath, content) {
+function saveFile(event, filePath, content) {
   fs.writeFile(filePath, content, function(err) {
     event.sender.send('file-saved', err);
   });
