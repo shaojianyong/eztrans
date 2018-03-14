ddfcde597a2f3db0aa16352f3e12776083136f9f
John Shao <shaojianyong@hotmail.com>
2018-03-14 13:21:22 +0800

    modify engine icons

--- src/app/providers/manager/engines.ts
+++ src/app/providers/manager/engines.ts
@@ -7,21 +7,21 @@ export default {
   Google: {
     name: 'Google',
     site: 'https://translate.google.cn',
-    icon: 'blue cloud icon'
+    icon: 'blue google icon'
   },
   Baidu: {
     name: 'Baidu',
     site: 'http://fanyi.baidu.com',
-    icon: 'green cloud icon'
+    icon: 'blue paw icon'
   },
   BaiduVIP: {
     name: 'Baidu-VIP',
     site: 'http://api.fanyi.baidu.com/api/trans/vip/translate',
-    icon: 'purple cloud icon'
+    icon: 'purple paw icon'
   },
   iCIBA: {
     name: 'iCiba',
     site: 'http://fy.iciba.com',
-    icon: 'orange cloud icon'
+    icon: 'blue cloud icon'
   }
 };
