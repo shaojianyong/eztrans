13c8511a157787b3ad9aaa5494017582ffba46bf
John Shao <shaojianyong@hotmail.com>
2018-04-07 17:02:43 +0800

    temp

--- src/app/services/model/group.model.ts
+++ src/app/services/model/group.model.ts
@@ -10,12 +10,14 @@ export class GroupModel {
   id: string;     // 分组标识
   name: string;   // 分组名称
   type: string;   // 分组类型
+  open: boolean;  // 是否展开
   documents: Array<DocInfoModel>;  // 组内文档
 
   constructor(obj?: any) {
     this.id = obj && obj.id || 'my-translations';
     this.name = obj && obj.name || 'My Translations';
     this.type = obj && obj.type || GroupType.CLIP;
+    this.open = obj && obj.open || false;
     this.documents = obj && obj.documents || [];
   }
 }
