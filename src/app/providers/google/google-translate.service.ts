77ad3c38f7357c6f4f217feb5025e97dc08ccd69
John Shao <shaojianyong@hotmail.com>
2018-04-17 15:14:14 +0800

    node-google-translate-skidz --> google-translate-api

--- src/app/providers/google/google-translate.service.ts
+++ src/app/providers/google/google-translate.service.ts
@@ -7,7 +7,7 @@ import { DocInfoModel } from '../../services/model/doc-info.model';
 
 // 由于translate.google.com被墙，需要手动修改node_modules中的代码，这不符合规矩
 // TODO: Fork项目node-google-translate-skidz，发布针对中国大陆的版本
-const google_translate = (<any>window).require('node-google-translate-skidz');
+const google_translate = (<any>window).require('google-translate-api');
 
 @Injectable()
 export class GoogleTranslateService extends TranslateService {
@@ -18,27 +18,13 @@ export class GoogleTranslateService extends TranslateService {
 
   translateX(srcText: string, translate: TranslateModel, docInfo: DocInfoModel): Observable<TranslateResult> {
     return Observable.create(observer => {
-      try {
-        google_translate({
-          text: srcText,
-          source: docInfo.source_lang,
-          target: docInfo.target_lang
-        }, (result) => {
-            translate.target_text = result.translation;
-          translate.trans_grade = 4;
-          observer.next({result: 'ok', doc_id: docInfo.id});
-        });
-      } catch (e) {
-        observer.error({result: e, doc_id: docInfo.id});
-      }
+      google_translate(srcText, {from: docInfo.source_lang, to: docInfo.target_lang}).then(res => {
+        translate.target_text = res.text;
+        translate.trans_grade = 4;
+        observer.next({result: 'ok', doc_id: docInfo.id});
+      }).catch(err => {
+        observer.error({result: err, doc_id: docInfo.id});
+      });
     });
   }
-
-  /*
-  translate(srcText: string, callback: Function): void {
-    google_translate({text: srcText, source: this.source_lang, target: this.target_lang}, (result) => {
-      // 中文拼音： result.sentences[result.sentences.length - 1].translit
-      callback(result.translation);
-    });
-  }*/
 }
