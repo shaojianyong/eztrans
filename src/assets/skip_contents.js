76149d61792c4d13475b9eb4917b6381c6bef780
John Shao <shaojianyong@hotmail.com>
2018-04-24 17:43:54 +0800

    gth2

--- src/assets/skip_contents.js
+++ src/assets/skip_contents.js
@@ -3,21 +3,12 @@ const SKIP_CONTENTS = [
   '\u005b-\u0060',  // 开方括号-开单引号
   '\u007b-\u007f',  // 开花括号-删除
   '\u0080-\u00ff',  // 拉丁文补充1：https://unicode-table.com/cn/blocks/latin-1-supplement/
-  '\u0100-\u017f',  // 拉丁文扩展A
+  '\u0100-\u017f',  // 拉丁文扩展A：https://unicode-table.com/cn/blocks/latin-extended-a/
+  '\u0180-\u024f',  // 拉丁文扩展B：https://unicode-table.com/cn/blocks/latin-extended-b/
+  '\u0250-\u02af',  // 国际音标扩展：https://unicode-table.com/cn/blocks/ipa-extensions/
 
 
-  '\u25b2\u25ba\u25bc\u25c4\u2794\u2799\u279c',  // 箭头符号(1)
-  '\u279e-\u27a0',  // 箭头符号(2)
-  '\u27a4-\u27a8\u27b2\u27b3\u27b5\u27b8\u27ba',  // 箭头符号(3)
-  '\u27bc-\u27be\u25a3\u25c6\u25c8',  // 箭头符号(4)
-  '\u2600-\u2603\u2605\u260e\u260f\u2618\u2620',  // 基本形状(1)
-  '\u2622-\u2625\u262a\u262e\u262f',  // 基本形状(2)
-  '\u2638-\u263b\u267b\u267f\u2693\u26a0\u26a1\u2702\u2704\u2706\u2708\u2709\u2726',  // 基本形状(3)
-  '\u273f-\u2742\u2744\u2748\u2764\u2766\u9749\ufffd',  // 基本形状(4)
-  '\u00a2-\u00a5',  // 货币符号
-  '\u00bc-\u00be\u2030\u2053\u2054',  // 数字类(1)
-  '\u205b-\u205d',  // 数字类(2)
-  '\u2669-\u266f',  // 音乐符号
+
 
 
 
@@ -28,9 +19,23 @@ const SKIP_CONTENTS = [
   '\u2190-\u21ff',  // 箭头：https://unicode-table.com/cn/blocks/arrows/
   '\u2200-\u22ff',  // 数学运算符：https://unicode-table.com/cn/blocks/mathematical-operators/
   '\u2460-\u24ff',  // 带圈或括号的字母数字：https://unicode-table.com/cn/blocks/enclosed-alphanumerics/
+  '\u2500-\u257f',  // 制表符：https://unicode-table.com/cn/blocks/box-drawing/
+  '\u2580-\u259f',  // 方块元素：https://unicode-table.com/cn/blocks/block-elements/
+  '\u25a0-\u25ff',  // 几何图形：https://unicode-table.com/cn/blocks/geometric-shapes/
+  '\u2600-\u26ff',  // 杂项符号：https://unicode-table.com/cn/blocks/miscellaneous-symbols/
+  '\u2700-\u27bf',  // 印刷符号：https://unicode-table.com/cn/blocks/dingbats/
+  '\u27c0-\u27ef',  // 杂项数学符号A：https://unicode-table.com/cn/blocks/miscellaneous-mathematical-symbols-a/
   '\u27f0-\u27ff',  // 追加箭头A：https://unicode-table.com/cn/blocks/suplemental-arrows-a/
+  '\u2800-\u28ff',  // 盲文点字模型：https://unicode-table.com/cn/blocks/braille-patterns/
   '\u2900-\u297f',  // 追加箭头B：https://unicode-table.com/cn/blocks/suplemental-arrows-b/
-  '\u200b'  // ZERO WIDTH SPACE
+  '\u2980—\u29ff',  // 杂项数学符号B：https://unicode-table.com/cn/blocks/miscellaneous-mathematical-symbols-b/
+  '\u2a00-\u2aff',  // 追加数学运算符：https://unicode-table.com/cn/blocks/supplemental-mathematical-operators/
+  '\u2b00-\u2bff',  // 杂项符号和箭头：https://unicode-table.com/cn/blocks/miscellaneous-symbols-and-arrows/
+  '\u2c60-\u2c7f',  // 拉丁文扩展C：https://unicode-table.com/cn/blocks/latin-extended-c/
+  '\u2e00-\u2e7f',  // 追加标点：https://unicode-table.com/cn/blocks/supplemental-punctuation/
+  '\u2ff0-\u2fff',  // 表意文字描述符：https://unicode-table.com/cn/blocks/ideographic-description-characters/
+  '\u3000-\u303f',  // 中日韩符号和标点：https://unicode-table.com/cn/blocks/cjk-symbols-and-punctuation/
+
 ];
 
 const SKIP_CONTENTS_REGEX = eval('/^[' + SKIP_CONTENTS.join('') + ']+$/');
